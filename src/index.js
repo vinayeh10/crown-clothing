39ed6e056d84a3ea234964b18d26125b413da84f
Vinay K.N <vinay.kn@epicor.com>
2021-06-01 23:45:20 +0530

    expense logger base setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
