ab2a5dbbc744375bee83ddf9b4bff046f0be535b
Vinay K.N <vinay.kn@epicor.com>
2021-07-08 20:27:06 +0530

    Created project with create react app and added homepage

---
+++ React-projects/build/static/js/6.b5fc077b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpexpense-logger"]=this["webpackJsonpexpense-logger"]||[]).push([[6],{335:function(e,n){},389:function(e,n){},390:function(e,n){}}]);
+//# sourceMappingURL=6.b5fc077b.chunk.js.map
