c192a2b12a10576e0f58d7f906ff4a57006ae3ac
Vinay K.N <vinay.kn@epicor.com>
2021-07-16 01:37:35 +0530

    header added to applications containing shop, contact

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SHOP from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
-    <Route exact path="/" component={Homepage}></Route>
-    <Route exact path="/shop" component={SHOP}></Route>    
+    <Header/>
+    <Switch>
+      <Route exact path="/" component={Homepage}></Route>
+      <Route exact path="/shop" component={SHOP}></Route>    
+    </Switch>
     </div>
   );
 }
