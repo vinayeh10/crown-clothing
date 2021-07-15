942aa7ece9249a1c4424ed88c36158f494bdfb40
Vinay K.N <vinay.kn@epicor.com>
2021-07-15 18:58:53 +0530

    updated menu-item to use withrouter HOC for routing

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
+
+const HatsPage = () => {
+  return (
+    <div>Hats Page</div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-    <Homepage />
+    <Route exact path="/" component={Homepage}></Route>
+    <Route exact path="/shop/hats" component={HatsPage}></Route>    
     </div>
   );
 }
+
 
 export default App;
--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
   return (
     <div className={`${size} menu-item`}
-    >
+    onClick={() => history.push(`${match.url}${linkUrl}`)} >
       <div style={{ backgroundImage: `url(${imageUrl})` }} className="background-image"></div>
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
@@ -14,4 +15,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
