8d762f271efd1bbe8a3c2158ea579441ee439ed7
Vinay K.N <vinay.kn@epicor.com>
2021-07-27 18:52:55 +0530

    added cart icon and cart dropdown

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import {ReactComponent as Logo} from '../../assets/crown.svg';
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => {
+const Header = ({currentUser, hidden}) => {
     return (
         <div className='header'>
             <Link to="/" className='logo-container'>
@@ -20,13 +22,16 @@ const Header = ({currentUser}) => {
                     :
                     <Link to='/signin' className='option'>SIGN IN</Link>
                 }
+                <CartIcon />
             </div>
+            {hidden ? null : <CartDropdown />}
         </div>
     );
 };
 
 const mapStateProps = state => ({
-    currentUser: state.user.currentUser
+    currentUser: state.user.currentUser,
+    hidden: state.cart.hidden
 });
 
 export default connect(mapStateProps, null)(Header);
