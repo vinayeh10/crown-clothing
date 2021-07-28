d2a5260a89561f2bf9971447c719efbb6d658ce7
Vinay K.N <vinay.kn@epicor.com>
2021-07-28 13:27:48 +0530

    updating application with reselect application cart selectors

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 
-const CartDropdown = props => {
+const CartDropdown = ({cartItems}) => {
     return (
         <div className='cart-dropdown'>
-            <div className='cart-items' />
+            <div className='cart-items'>
+                {
+                    cartItems.map(item => <CartItem key={item.id} item={item} />)
+                }
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     );
 };
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
+})
+
+export default connect(mapStateToProps, null)(CartDropdown);
